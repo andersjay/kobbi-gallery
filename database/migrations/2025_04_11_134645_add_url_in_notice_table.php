63eb40164f0aa313b0553000907a8f009cb60891
Anderson Leite <anderson07102010@hotmail.com>
2025-05-11 21:13:16 -0300

    Update 2025_04_11_134645_add_url_in_notice_table.php

--- database/migrations/2025_04_11_134645_add_url_in_notice_table.php
+++ database/migrations/2025_04_11_134645_add_url_in_notice_table.php
@@ -11,9 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('noticies', function (Blueprint $table) {
-            $table->string('url')->nullable();
-        });
+        if (!Schema::hasColumn('noticies', 'url')) {
+            Schema::table('noticies', function (Blueprint $table) {
+                $table->string('url')->nullable();
+            });
+        }
     }
 
     /**
@@ -21,8 +23,10 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('noticies', function (Blueprint $table) {
-            $table->dropColumn('url');
-        });
+        if (Schema::hasColumn('noticies', 'url')) {
+            Schema::table('noticies', function (Blueprint $table) {
+                $table->dropColumn('url');
+            });
+        }
     }
 };
