8a35209bdba0b839a5dd98277ed9a3dfcaf90f48
Anderson Leite <anderson07102010@hotmail.com>
2025-06-13 19:28:02 -0300

    Atualiza campos de obras nas exposições

--- resources/views/emails/interesse-exposicao.blade.php
+++ resources/views/emails/interesse-exposicao.blade.php
@@ -29,17 +29,11 @@
                             @if(isset($obra['name']))
                                 <p><strong>Nome:</strong> {{ $obra['name'] }}</p>
                             @endif
-                            @if(isset($obra['year']))
-                                <p><strong>Ano:</strong> {{ $obra['year'] }}</p>
-                            @endif
-                            @if(isset($obra['technique']))
-                                <p><strong>Técnica:</strong> {{ $obra['technique'] }}</p>
-                            @endif
-                            @if(isset($obra['size']))
-                                <p><strong>Tamanho:</strong> {{ $obra['size'] }}</p>
+                            @if(isset($obra['artist']))
+                                <p><strong>Artista:</strong> {{ $obra['artist'] }}</p>
                             @endif
                             @if(isset($obra['description']))
-                                <p><strong>Descrição:</strong> {{ $obra['description'] }}</p>
+                                <p><strong>Descrição:</strong> {!! $obra['description'] !!}</p>
                             @endif
                             @if(isset($obra['image']))
                                 <p>
