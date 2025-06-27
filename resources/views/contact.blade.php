d4cd01de772a4fb8cf1fa8d4ca3d9a86584408aa
Anderson Leite <anderson07102010@hotmail.com>
2025-06-27 16:58:09 -0300

    fix

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container-kobbi  pt-14 mx-auto pb-10">
+    <div class="container-kobbi pt-14 mx-auto pb-10 md:px-[6rem!important]">
         <h1 class="header-title-spacing text-3xl text-gray-950 font-light">CONTATO</h1>
 
         <div class="mt-8">
             <h2 class="text-2xl text-gray-950 font-normal mb-8">KOBBI GALLERY</h2>
         </div>
-        <div class="grid md:grid-cols-2 gap-16 mt-12">
+        <div class=" md:grid md:grid-cols-2 flex flex-col gap-16 mt-8 ">
             <!-- Seções Personalizadas de Contato -->
-            <div class="space-y-12">
+            <div class="space-y-8 ml-4">
                 @if ($contactSettings && $contactSettings->section1_title)
                     <div>
                         <h2 class="text-xl text-gray-950 font-normal mb-4">{{ $contactSettings->section1_title }}</h2>
@@ -32,12 +32,14 @@
             </div>
 
             <!-- Mapa -->
-            <div class="h-[400px] overflow-hidden">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d306.4981110836952!2d-46.686229221082236!3d-23.556123530287405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce579576ede819%3A0x72411ec7188e8c53!2sKobbi%20Gallery!5e0!3m2!1spt-BR!2sbr!4v1749851985241!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <div class="flex items-start">
+                <div class="w-full h-[400px] overflow-hidden">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d978.6841387468653!2d-46.686229221082236!3d-23.556123530287405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce579576ede819%3A0x72411ec7188e8c53!2sKobbi%20Gallery!5e0!3m2!1spt-BR!2sbr!4v1749851985241!5m2!1spt-BR!2sbr" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
             </div>
         </div>
 
-        <div class="mt-16 border-t border-gray-300 pt-12">
+        <div class="mt-12 border-t border-gray-300 pt-8">
             <!-- Formulário de Contato -->
             <div class="space-y-6">
                 <h2 class="text-xl text-gray-950 font-normal mb-6">ENVIE SUA MENSAGEM</h2>
@@ -72,14 +74,14 @@
 
          <!-- Lista da Equipe -->
         @if($teams && $teams->count() > 0)
-        <div class="mt-16 pt-12 border-t border-gray-300">
+        <div class="mt-12 pt-8 border-t border-gray-300">
             <div class="flex items-center gap-4 mb-8">
                 <h2 class="text-xl text-gray-950 font-normal">EQUIPE</h2>
                 <div class="h-[1px] w-full bg-gray-300"></div>
             </div>
             <div class="space-y-6">
                 @foreach($teams as $member)
-                    <div class="mb-8">
+                    <div class="mb-6">
                         <div class="text-gray-950 text-base font-medium tracking-wide uppercase mb-2">{{ $member->function }}</div>
                         <div class="text-gray-700 text-base">{{ $member->name }}</div>
                     </div>
